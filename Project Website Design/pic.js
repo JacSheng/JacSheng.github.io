8aa83bdb9a4c2d67f8fa3f4c563a95c117f5a1a9
JacSheng <122499888+JacSheng@users.noreply.github.com>
2023-03-09 21:06:16 +0800

    Delete Project Website Design directory

--- Project Website Design/pic.js
+++
@@ -1,8 +0,0 @@
-
-    let imgs =document.getElementsByTagName('img');
-    for(let i=1;i<imgs.length;i++){
-        imgs[i].onmouseover = function(){
-            imgs[0].src = this.src;
-        }
-    }
-    
